60ed46c1ae9c9edc37a3c4b355119e0df23572f1
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-14 21:41:04 -0300

    feat: introduce initial app and labels

---
+++ DogAge/DogAge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogAge
+//
+//  Created by Rhian Lopes da Costa on 14/07/20.
+//  Copyright © 2020 Rhian Lopes da Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
